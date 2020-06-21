58720738cb86d4349111206576536b7dad145f1f
Adam Bystran <bystran.adam.21@gmail.com>
2020-06-21 18:17:46 +0200

    Fixing queue worker, to execute jobs until empty.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,6 +24,6 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('queue:work --daemon')->everyMinute()->withoutOverlapping();
+        $schedule->command('queue:work --stop-when-empty')->everyMinute()->withoutOverlapping();
     }
 }
