9f87938294b10bca80dd30723a941ad5af62f9a7
Adam Bystran <bystran.adam.21@gmail.com>
2020-06-22 12:37:07 +0200

    removes checkbox

--- app/Http/Controllers/ContactFormController.php
+++ app/Http/Controllers/ContactFormController.php
@@ -32,7 +32,6 @@ class ContactFormController extends Controller
             'last_name'=>'required|alpha|max:50|min:2',
             'email'=>'required|email',
             'message'=>'required|min:10|max:500',
-            'checkbox' =>'accepted',
             'g-recaptcha-response' => 'required|captcha'
         ];
 
